2865fb45bf8898f3a49203366cbfd3ed3d7859d0
Sergey <xxsmotur@gmail.com>
2019-12-15 16:26:17 +0500

    fix arguments

--- torchnn/cgan/cgan.py
+++ torchnn/cgan/cgan.py
@@ -43,12 +43,12 @@ class Discriminator(nn.Module):
         return validity
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--d1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--d2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--cpus", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
 parser.add_argument("--classes", type=int, default=10, help="number of classes for dataset")
 opt = parser.parse_args()
