0b7600beb23707b47fb14a111e625bee20a867db
Sergey <xxsmotur@gmail.com>
2019-12-15 16:24:15 +0500

    Addign arguments

--- torchnn/cgan/cgan.py
+++ torchnn/cgan/cgan.py
@@ -41,3 +41,27 @@ class Discriminator(nn.Module):
         d_in = torch.cat((img.view(img.size(0), -1), self.label_embedding(labels)), -1)
         validity = self.model(d_in)
         return validity
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--d1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--d2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--classes", type=int, default=10, help="number of classes for dataset")
+opt = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+adversarial_loss = torch.nn.MSELoss()
+
+generator = Generator()
+discriminator = Discriminator()
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
