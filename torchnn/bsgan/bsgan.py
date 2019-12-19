280e03a2ef78c56ad63c59c11c061df1fcb0a224
Sergey <xxsmotur@gmail.com>
2019-12-19 19:07:48 +0500

    Add training method

--- torchnn/bsgan/bsgan.py
+++ torchnn/bsgan/bsgan.py
@@ -73,3 +73,59 @@ parser.add_argument("--channels", type=int, default=1, help="image channels")
 parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
 opt = parser.parse_args()
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+adversarial_loss = torch.nn.MSELoss()
+generator = Generator(opt.classes, opt.latent_dim, (opt.channels, opt.img_size, opt.img_size))
+discriminator = Discriminator(opt.classes, opt.latent_dim, (opt.channels, opt.img_size, opt.img_size))
+
+trainset = datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.Resize(opt.img_size),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ]))
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
+
+for epoch in range(opt.epochs):
+    for i, (imgs, labels) in enumerate(trainloader):
+        batch_size = imgs.shape[0]
+
+        valid = Variable(torch.FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
+        fake = Variable(torch.FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
+        img = Variable(imgs.type(torch.FloatTensor))
+        labels = Variable(labels.type(torch.LongTensor))
+        optimizer_G.zero_grad()
+
+        z = Variable(torch.FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
+        gen_labels = Variable(torch.LongTensor(np.random.randint(0, opt.classes, batch_size)))
+        gen_imgs = generator(z, gen_labels)
+        bs_loss = boundary_loss(discriminator(gen_imgs, gen_labels), valid)
+        validity = discriminator(gen_imgs, gen_labels)
+        g_loss = adversarial_loss(validity, valid)
+
+        optimizer_D.zero_grad()
+        disk_images = discriminator(img, labels)
+        a_loss_real = adversarial_loss(validity, valid)
+
+        validity_fake = discriminator(gen_imgs.detach(), gen_labels)
+        a_loss_fake = adversarial_loss(validity_fake, fake)
+
+        d_loss = (a_loss_real + a_loss_fake) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+            % (epoch, opt.epochs, i, len(trainloader), d_loss.item(), g_loss.item())
+        )
+
+        batches_done = epoch * len(trainloader) + i
+        #if batches_done % opt.sample_interval == 0:
+        #    sample_image(n_row=10, batches_done=batches_done)
+
