63a435e20f6b2a86c52b2881b041a78748c60391
Sergey <xxsmotur@gmail.com>
2019-12-21 23:59:36 +0500

    Draft for training algorithm

--- torchnn/vae/vae.py
+++ torchnn/vae/vae.py
@@ -29,3 +29,38 @@ class VAE(nn.Module):
         mu, logvar = self._encode(x)
         z = self._reparameterize(mu, logvar)
         return self._decode(z), mu, logvar
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--d1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--d2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--cpus", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--classes", type=int, default=10, help="number of classes for dataset")
+parser.add_argument("--channels", type=int, default=1, help="image channels")
+parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
+opt = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+adversarial_loss = torch.nn.MSELoss()
+generator = Generator(opt.classes, opt.latent_dim, (opt.channels, opt.img_size, opt.img_size))
+discriminator = Discriminator(opt.classes, opt.latent_dim, (opt.channels, opt.img_size, opt.img_size))
+
+trainset = datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.Resize(opt.img_size),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ]))
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
+
+for epoch in range(opt.epochs):
+    for i, (imgs, labels) in enumerate(trainloader):
+        pass
