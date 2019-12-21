2d6bd5c591bdfd9a38885be936d001e21cd48b58
Sergey <xxsmotur@gmail.com>
2019-12-21 23:51:34 +0500

    reparatramization of layers

--- torchnn/vae/vae.py
+++ torchnn/vae/vae.py
@@ -4,13 +4,11 @@ import torch.nn.functional as F
 class VAE(nn.Module):
     def __init__(self, input_dim):
         super(Generator, self).__init__()
-        self.model = nn.Sequential(
-            *self._block(input_dim, 300),
-            *self._block(300, 20),
-            *self._block(300, 20),
-            *self._block(20, 300),
-            *self._block(300, input_dim),
-        )
+        self.layer1 = self._block(input_dim, 300)
+        self.layer2 = self._block(300, 20)
+        self.layer3 = self._block(300, 20)
+        self.layer4 = self._block(20, 300)
+        self.layer5 = self._block(300, input_dim)
     
     def _block(self, in_features, out_features, batch_norm=0.8):
         return nn.Linear(in_features, out_features)
