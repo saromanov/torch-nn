1f1c05728deba1779a8da2f2345dee9a6bf682fd
Sergey <xxsmotur@gmail.com>
2019-12-24 17:05:37 +0500

    Adding argparse

--- torchnn/nn/nn.py
+++ torchnn/nn/nn.py
@@ -10,8 +10,7 @@ class NN(nn.Module):
         super(NN, self).__init__()
         self.model = nn.Sequential(
             *self._make_module(inp,hidd),
-            *self._make_module(hidd,out),
-            nn.Sigmoid()
+            *self._make_module(hidd,out)
         )
     
     def _make_module(self, inNum, outNum):
@@ -20,15 +19,22 @@ class NN(nn.Module):
     def forward(self, x):
         return self.model(x)
 
-x = torch.randn(10000, 100)
-y = torch.randn(10000, 20)
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--input_dim", type=int, default=100, help="dimensionality of the input space")
+parser.add_argument("--hidden_dim", type=int, default=50, help="dimensionality of the hidden space")
+parser.add_argument("--out_dim", type=int, default=20, help="dimensionality of the output space")
+opt = parser.parse_args()
+
+x = torch.randn(10000, opt.input_dim)
+y = torch.randn(10000, opt.out_dim)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = NN(100,50,20).to(device)
+model = NN(opt.input_dim,opt.hidden_dim, opt.out_dim).to(device)
 loss = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-for i in range(1000):
+for i in range(opt.epochs):
     pred = model(x)
 
     loss_fn = loss(pred, y)
