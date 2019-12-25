fa9df877a73e32d7ed0ae8274f19539f6337803e
Sergey <xxsmotur@gmail.com>
2019-12-25 19:08:31 +0500

    Implementation of conv gan layers

---
+++ torchnn/convgan/convgan.py
@@ -0,0 +1,63 @@
+
+import argparse
+import torch
+from torchvision import datasets
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+class Generator(nn.Module):
+    ''' Definition of the generator class
+    '''
+    def __init__(self, num_classes, dims, shape):
+        super(Generator, self).__init__()
+        self._shape = shape
+        self.labels_emb = nn.Embedding(num_classes, num_classes)
+        self.model = nn.Sequential(
+            nn.BatchNorm2d(1),
+            nn.ReLU(True)
+        )
+
+        self.downsample1 = nn.Sequential(
+            nn.Conv2d(1, 50, 3, stride=1, padding=1),
+            nn.BatchNorm2d(50),
+            nn.ReLU(True)
+        )
+        self.downsample2 = nn.Sequential(
+            nn.Conv2d(50, 25, 3, stride=1, padding=1),
+            nn.BatchNorm2d(25),
+            nn.ReLU(True)
+        )
+        self.downsample3 = nn.Sequential(
+            nn.Conv2d(25, 1, 2, stride=2),
+            nn.Tanh()
+        )
+    
+    def forward(self, noise, labels):
+        gen_input = torch.cat((self.labels_emb(labels), noise), -1)
+        img = self.model(gen_input)
+        img = img.view(img.size(0), *self._shape)
+        return img
+
+class Discriminator(nn.Module):
+    def __init__(self, num_classes, dims, shape):
+        super(Discriminator, self).__init__()
+        self._shape = shape
+        self._labels_emb = nn.Embedding(num_classes, num_classes)
+        self.model = nn.Sequential(
+            nn.Linear(num_classes + int(np.prod(shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.Dropout(0.4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.Softplus(0.5),
+            nn.Linear(512, 1),
+        )
+    
+    def forward(self, img, labels):
+        d_in = torch.cat((img.view(img.size(0), -1), self._labels_emb(labels)), -1)
+        validity = self.model(d_in)
+        return validity
