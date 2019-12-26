b7f9af35f997833ca7f805426c735bc9c3839942
Sergey <xxsmotur@gmail.com>
2019-12-26 23:19:14 +0500

    implementing of discriminator

--- torchnn/convgan/convgan.py
+++ torchnn/convgan/convgan.py
@@ -46,20 +46,23 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self, num_classes, dims, shape):
         super(Discriminator, self).__init__()
-        self._shape = shape
-        self._labels_emb = nn.Embedding(num_classes, num_classes)
-        self.model = nn.Sequential(
-            nn.Linear(num_classes + int(np.prod(shape)), 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Softplus(0.5),
-            nn.Linear(512, 1),
+        self.fc = nn.Sequential(
+            nn.Linear(64*7*7, 1024),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        )
+        self.conv1 = self._make_conv_layer(1,32,5)
+        self.conv2 = self._make_conv_layer(32,64,5)
+    
+    def _make_conv_layer(x,y,z):
+        return nn.Sequential(
+            nn.Conv2d(x, y, z, padding=2),
+            nn.LeakyReLU(0.2, True),
+            nn.MaxPool2d(2),
         )
     
-    def forward(self, img, labels):
-        d_in = torch.cat((img.view(img.size(0), -1), self._labels_emb(labels)), -1)
-        validity = self.model(d_in)
-        return validity
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return self.fc(x.view(x.size(0), -1))
