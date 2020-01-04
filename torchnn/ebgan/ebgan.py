b05b856243d255d6250660cbc3cd845be1b68af1
Sergey <xxsmotur@gmail.com>
2020-01-05 02:08:28 +0500

    Add class for ebgan

--- torchnn/ebgan/ebgan.py
+++ torchnn/ebgan/ebgan.py
@@ -19,13 +19,15 @@ class Discriminator(nn.Module):
     '''
     def __init__(self, channels, size):
         super(Discriminator, self).__init__()
-        self._downsampling = nn.Sequential(
+        #Definition of the downsample layer
+        self.downsampling = nn.Sequential(
             nn.Conv2d(opt.channels, 64, 3, 2, 1),
             nn.ReLU()
         )
         self._dim = self._create_down(size)
         self._down_size = opt.img_size // 2
         self.embedding = nn.Linear(self._dim, 32)
+        #FC layer
         self.fc = nn.Sequential(
             nn.BatchNorm1d(32, 0.8),
             nn.ReLU(inplace=True),
@@ -33,13 +35,17 @@ class Discriminator(nn.Module):
             nn.BatchNorm1d(self._dim),
             nn.ReLU(inplace=True),
         )
-        self.up = nn.Sequential(nn.Upsample(scale_factor=2), nn.Conv2d(64, opt.channels, 3, 1, 1))
+        # defintion of upsample layer
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2), 
+            nn.Conv2d(64, opt.channels, 3, 1, 1)
+        )
     
     def _create_down(self, size):
         return 64 * (opt.img_size // 2) ** 2 
     
     def forward(self, img):
-       out = self._downsampling(img)
+       out = self.downsampling(img)
        embedding = self.embedding(out.view(out.size(0), -1))
        out = self.fc(embedding)
        out = self.up(out.view(out.size(0), 64, self._down_size, self._down_size))
@@ -80,6 +86,53 @@ def pullaway_loss(embeddings):
     loss_pt = (torch.sum(similarity) - batch_size) / (batch_size * (batch_size - 1))
     return loss_pt
 
+
+class EBGAN:
+    def __init__(self):
+        pass
+    
+    def train(self, epochs, lambda_pt=0.1):
+        optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
+        optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
+        margin = max(1, opt.batch_size / 64.0)
+        for epoch in range(opt.epochs):
+            for i, (imgs, _) in enumerate(trainloader):
+                batch_size = imgs.shape[0]
+
+                real = Variable(imgs.type(torch.FloatTensor))
+                optimizer_G.zero_grad()
+
+                z = Variable(torch.FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
+                gen_imgs = generator(z)
+                recon_imgs, img_embeddings = discriminator(gen_imgs)
+
+                g_loss = pixelwise_loss(recon_imgs, gen_imgs.detach()) + lambda_pt * pullaway_loss(img_embeddings)
+
+                g_loss.backward()
+                optimizer_G.step()
+                optimizer_D.zero_grad()
+                
+                disk_images,_ = discriminator(real)
+                validity_fake,_ = discriminator(gen_imgs.detach())
+                a_loss_real = pixelwise_loss(disk_images, real)
+                a_loss_fake = pixelwise_loss(validity_fake, gen_imgs.detach())
+
+                d_loss = a_loss_real
+                if (margin - a_loss_fake.data).item() > 0:
+                    d_loss += margin - a_loss_fake
+
+                d_loss.backward()
+                optimizer_D.step()
+
+                print(
+                    "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                    % (epoch, opt.epochs, i, len(trainloader), d_loss.item(), g_loss.item())
+                )
+
+                batches_done = epoch * len(trainloader) + i
+                #if batches_done % opt.sample_interval == 0:
+                #    sample_image(n_row=10, batches_done=batches_done)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
@@ -95,6 +148,7 @@ opt = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 pixelwise_loss = torch.nn.MSELoss()
+
 generator = Generator(opt.channels, opt.latent_dim, opt.img_size)
 discriminator = Discriminator(opt.channels, opt.img_size)
 
@@ -106,48 +160,3 @@ trainset = datasets.MNIST('../data', train=True, download=True,
                        ]))
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
-
-print(trainset[0][0].size())
-
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.d1, opt.d2))
-
-lambda_pt = 0.1
-margin = max(1, opt.batch_size / 64.0)
-for epoch in range(opt.epochs):
-    for i, (imgs, _) in enumerate(trainloader):
-        batch_size = imgs.shape[0]
-
-        real = Variable(imgs.type(torch.FloatTensor))
-        optimizer_G.zero_grad()
-
-        z = Variable(torch.FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
-        gen_imgs = generator(z)
-        recon_imgs, img_embeddings = discriminator(gen_imgs)
-
-        g_loss = pixelwise_loss(recon_imgs, gen_imgs.detach()) + lambda_pt * pullaway_loss(img_embeddings)
-
-        g_loss.backward()
-        optimizer_G.step()
-        optimizer_D.zero_grad()
-        
-        disk_images,_ = discriminator(real)
-        validity_fake,_ = discriminator(gen_imgs.detach())
-        a_loss_real = pixelwise_loss(disk_images, real)
-        a_loss_fake = pixelwise_loss(validity_fake, gen_imgs.detach())
-
-        d_loss = a_loss_real
-        if (margin - a_loss_fake.data).item() > 0:
-            d_loss += margin - a_loss_fake
-
-        d_loss.backward()
-        optimizer_D.step()
-
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.epochs, i, len(trainloader), d_loss.item(), g_loss.item())
-        )
-
-        batches_done = epoch * len(trainloader) + i
-        #if batches_done % opt.sample_interval == 0:
-        #    sample_image(n_row=10, batches_done=batches_done)
